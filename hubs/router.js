ff6636309905c3faec49ada054decb4ed1b7cc89
jacobcalv <jacobcalvino@outlook.com>
2019-12-09 20:37:35 -0600

    finished all request now have to add specifications to them

--- hubs/router.js
+++ hubs/router.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const db = require("../data/db")
 
+//Get request for all posts
 router.get('/', async (req,res) => {
     try{
         const posts = await db.find(req.query); 
@@ -15,6 +16,7 @@ router.get('/', async (req,res) => {
     }
 })
 
+//get request for single id
 router.get('/:id', async (req,res) => {
     try{
         const posts = await db.findById(req.params.id); 
@@ -22,11 +24,25 @@ router.get('/:id', async (req,res) => {
     }catch (error) {
         console.log(error);
         res.status(500).json({
-            messages: 'Error retrieving posts for this database user'
+            messages: 'Error retrieving posts for this database id'
         })
     }
 })
 
+//get request for comments for single post
+router.get('/:id/comments', async (req,res) => {
+    try{
+        const comments = await db.findPostComments(req.params.id); 
+        res.status(200).json(comments);
+    }catch (error) {
+        console.log(error);
+        res.status(500).json({
+            messages: 'Error retrieving posts for this database id'
+        })
+    }
+})
+
+//post request for blog post
 router.post('/', async (req,res) => {
     const postInfo = req.body
     try{
@@ -35,6 +51,45 @@ router.post('/', async (req,res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({error})
+    }
+})
+
+//post request for comments
+router.post('/:id/comments', async (req,res) => {
+    const commentInfo = {...req.body, post_id: req.params.id}
+    try{
+        const comment = await db.insertComment(commentInfo); 
+        res.status(200).json(comment);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error})
+    }
+})
+
+//delete request for blog posts
+router.delete('/:id', async (req,res) => {
+    try{
+        const post = await db.remove(req.params.id); 
+        res.status(200).json(post);
+    }catch (error) {
+        console.log(error);
+        res.status(500).json({
+            messages: 'Error retrieving posts for this database id'
+        })
+    }
+})
+
+//put request for editing posts
+router.put('/:id', async (req,res) => {
+    // const updateInfo = (req.params.id, req.body)
+    try{
+        const updatedPost = await db.update(req.params.id, req.body); 
+        res.status(200).json(updatedPost);
+    }catch (error) {
+        console.log(error);
+        res.status(500).json({
+            messages: 'Error Updating posts for this database id'
+        })
     }
 })
 
