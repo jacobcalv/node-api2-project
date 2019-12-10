127254e889afe2c78de9b48c047eeb77f733077d
jacobcalv <jacobcalvino@outlook.com>
2019-12-09 20:05:24 -0600

    finished get request for all and id, also finished the post request for blog

--- hubs/router.js
+++ hubs/router.js
@@ -1,6 +1,41 @@
 const express = require('express');
 
 const router = express.Router();
-const hubs = require("../data/db")
+const db = require("../data/db")
+
+router.get('/', async (req,res) => {
+    try{
+        const posts = await db.find(req.query); 
+        res.status(200).json(posts);
+    }catch (error) {
+        console.log(error);
+        res.status(500).json({
+            messages: 'Error retrieving posts for this database'
+        })
+    }
+})
+
+router.get('/:id', async (req,res) => {
+    try{
+        const posts = await db.findById(req.params.id); 
+        res.status(200).json(posts);
+    }catch (error) {
+        console.log(error);
+        res.status(500).json({
+            messages: 'Error retrieving posts for this database user'
+        })
+    }
+})
+
+router.post('/', async (req,res) => {
+    const postInfo = req.body
+    try{
+        const posts = await db.insert(postInfo); 
+        res.status(200).json(posts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error})
+    }
+})
 
 module.exports = router;
